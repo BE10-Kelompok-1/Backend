77d038936a7c1e7a4f41829f5bfbdbd9c1686a17
lukmanhafidz <lukmanhafidz40@gmail.com>
2022-07-29 08:59:56 +0700

    update config

--- config/config.go
+++ config/config.go
@@ -5,8 +5,6 @@ import (
 	"os"
 	"strconv"
 	"sync"
-
-	"github.com/joho/godotenv"
 	// 	"github.com/joho/godotenv"
 )
 
@@ -38,12 +36,12 @@ func GetConfig() *AppConfig {
 
 func initConfig() *AppConfig {
 	var defaultConfig AppConfig
-	err := godotenv.Load("local.env")
+	// err := godotenv.Load("local.env")
 
-	if err != nil {
-		log.Fatal("Cannot read configuration")
-		return nil
-	}
+	// if err != nil {
+	// 	log.Fatal("Cannot read configuration")
+	// 	return nil
+	// }
 	SECRET = os.Getenv("SECRET")
 	cnv, err := strconv.Atoi(os.Getenv("SERVERPORT"))
 	if err != nil {
